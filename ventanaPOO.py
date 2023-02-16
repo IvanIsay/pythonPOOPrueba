4a373108622bfeec390c0c556403f16ac9850a05
lOkY <102552151+IvanIsay@users.noreply.github.com>
2023-02-16 16:53:25 -0600

    posiciones y mensajes

--- ventanaPOO.py
+++ ventanaPOO.py
@@ -1,28 +1,58 @@
+from tkinter import Tk,Button,Frame,messagebox
 
-from tkinter import Tk,Button,Frame
+#5.MessageBox
+def mostrarMensajes():
+    print(messagebox.showinfo("showinfo", "Information"))
+    print(messagebox.showerror("showerror", "Error"))
+    print(messagebox.askokcancel(message="¿Desea continuar?", title="Título"))
 
+
+
+#1.Ventana
 Ventana= Tk()
 Ventana.title("2 Frames")
 Ventana.geometry("500x300")
 
 
-
+#2.Secciones
 seccion1= Frame(Ventana,bg="red")
 seccion1.pack(expand= True, fill= 'both')
 
 seccion2= Frame(Ventana,bg="gray")
 seccion2.pack(expand= True, fill= 'both')
 
-botonazul= Button(seccion1,text="blue", fg="blue")
-botonazul.pack()
+
+#3.Botones 
+
+botonazul= Button(seccion1,text="blue", fg="blue",command=mostrarMensajes)
+botonazul.place(x=60,y=60,  width=100, height=30)
+
 
 botonnegro= Button(seccion2,text="black", fg="black")
-botonnegro.pack()
+botonnegro.configure(height=2,width=10)
+botonnegro.grid(row=0, column=1)
 
+botonamarillo= Button(seccion2,text="yellow", fg="yellow")
+botonamarillo.configure(height=2,width=5 )
+botonamarillo.grid(row=1, column=2)
+
+#4.Posicionamiento
+
+#Main
 Ventana.mainloop()
 
 
+'''
+showinfo()
+showwarning()
+showerror()
+askquestion()
+askyesno()
+askokcancel()
+askyesnocancel()
+askretrycancel()
+'''
 
-#1.Ventana
-#2.Secciones
-#3.Botones 
+
+
+
