e6bc8213ff1c8320df2446387a179a7252ed5a9a
lOkY <102552151+IvanIsay@users.noreply.github.com>
2023-02-19 14:00:38 -0600

    afinado detalles

--- ventanaPOO.py
+++ ventanaPOO.py
@@ -1,17 +1,23 @@
 from tkinter import Tk,Button,Frame,messagebox
 
+
 #5.MessageBox
 def mostrarMensajes():
-    print(messagebox.showinfo("showinfo", "Information"))
-    print(messagebox.showerror("showerror", "Error"))
+    messagebox.showinfo("showinfo", "Information")
+    messagebox.showerror("showerror", "Error")
     print(messagebox.askokcancel(message="¿Desea continuar?", title="Título"))
 
-
+#6.Agregar Botones  
+def agregarbotones():
+    botonverde.config(text="+")
+    botonNuevo= Button(seccion3,text="Nuevo", bg="green")
+    botonNuevo.configure(height=3,width=5 )
+    botonNuevo.pack()
 
 #1.Ventana
 Ventana= Tk()
-Ventana.title("2 Frames")
-Ventana.geometry("500x300")
+Ventana.title("3 Frames")
+Ventana.geometry("600x400")
 
 
 #2.Secciones
@@ -21,25 +27,36 @@ seccion1.pack(expand= True, fill= 'both')
 seccion2= Frame(Ventana,bg="gray")
 seccion2.pack(expand= True, fill= 'both')
 
+seccion3= Frame(Ventana,bg="pink")
+seccion3.pack(expand= True, fill= 'both')
+
+
 
 #3.Botones 
 
-botonazul= Button(seccion1,text="blue", fg="blue",command=mostrarMensajes)
+botonazul= Button(seccion1,text="azul", fg="blue",command=mostrarMensajes)
 botonazul.place(x=60,y=60,  width=100, height=30)
 
 
-botonnegro= Button(seccion2,text="black", fg="black")
+botonnegro= Button(seccion2,text="negro", fg="black")
 botonnegro.configure(height=2,width=10)
 botonnegro.grid(row=0, column=1)
 
-botonamarillo= Button(seccion2,text="yellow", fg="yellow")
+botonamarillo= Button(seccion2,text="amarillo", fg="yellow")
 botonamarillo.configure(height=2,width=5 )
 botonamarillo.grid(row=1, column=2)
+
+botonverde= Button(seccion3,text="verde", fg="green",command=agregarbotones )
+botonverde.configure(height=3,width=5 )
+botonverde.pack()
+
 
 #4.Posicionamiento
 
 #Main
 Ventana.mainloop()
+
+
 
 
 '''
